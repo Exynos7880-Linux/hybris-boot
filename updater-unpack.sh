ea184f3c4f35a88434185514e5710009913cb9a0
aymanrgab <aymanrar2c@gmail.com>
2022-10-20 20:45:53 +0200

    get ride of system.img

--- updater-unpack.sh
+++ updater-unpack.sh
@@ -7,10 +7,9 @@ rm -rf /data/.stowaways
 mkdir -p $IMG_DIR
 mkdir -p /data/.stowaways/sailfishos
 
-mv /data/sfosrootfs.img /data/.stowaways/images/rootfs.img
-mv /data/system.img /data/.stowaways/images/
-e2fsck -fy /data/.stowaways/images/rootfs.img
-resize2fs /data/.stowaways/images/rootfs.img 8G
+mv /data/rootfs.img $IMG_DIR/rootfs.img
+e2fsck -fy $IMG_DIR/rootfs.img
+resize2fs $IMG_DIR/rootfs.img 8G
 
 EXIT=$?
 
